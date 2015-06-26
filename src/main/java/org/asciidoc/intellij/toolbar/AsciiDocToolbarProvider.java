037926e62c4d1fe0f855ad0bb8503ddffb42b39d
Vojtech Krasa <vojta.krasa@gmail.com>
2015-06-26 22:44:10 +0200

    new toolbar, hide AsciiDoc group in context menu when not needed

---
+++ src/main/java/org/asciidoc/intellij/toolbar/AsciiDocToolbarProvider.java
@@ -0,0 +1,31 @@
+package org.asciidoc.intellij.toolbar;
+
+import com.intellij.openapi.actionSystem.DataProvider;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.ui.EditorNotifications;
+import org.asciidoc.intellij.AsciiDocLanguage;
+
+public class AsciiDocToolbarProvider extends EditorNotifications.Provider<AsciiDocToolbarPanel> {
+
+  private static final Key<AsciiDocToolbarPanel> KEY = Key.create("AsciiDocToolbarProvider");
+
+  public Key<AsciiDocToolbarPanel> getKey() {
+    return KEY;
+  }
+
+  public AsciiDocToolbarPanel createNotificationPanel(VirtualFile file, FileEditor fileEditor) {
+    if (fileEditor instanceof PsiAwareTextEditorImpl) {
+      DataProvider component = (DataProvider)fileEditor.getComponent();
+      PsiFile data = LangDataKeys.PSI_FILE.getData(component);
+      if (data!=null && data.getViewProvider().getBaseLanguage().isKindOf(AsciiDocLanguage.INSTANCE)) {
+        return new AsciiDocToolbarPanel();
+      }
+    }
+    return null;
+  }
+}
