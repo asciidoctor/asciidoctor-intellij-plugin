4de1011bdda8585bef65e492c042d280c3517ac4
Vojtech Krasa <vojta.krasa@gmail.com>
2015-06-26 23:02:08 +0200

    nicer instanceof

--- src/main/java/org/asciidoc/intellij/toolbar/AsciiDocToolbarProvider.java
+++ src/main/java/org/asciidoc/intellij/toolbar/AsciiDocToolbarProvider.java
@@ -3,7 +3,7 @@ package org.asciidoc.intellij.toolbar;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl;
+import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -19,10 +19,10 @@ public class AsciiDocToolbarProvider extends EditorNotifications.Provider<AsciiD
   }
 
   public AsciiDocToolbarPanel createNotificationPanel(VirtualFile file, FileEditor fileEditor) {
-    if (fileEditor instanceof PsiAwareTextEditorImpl) {
+    if (fileEditor instanceof TextEditor) {
       DataProvider component = (DataProvider)fileEditor.getComponent();
       PsiFile data = LangDataKeys.PSI_FILE.getData(component);
-      if (data!=null && data.getViewProvider().getBaseLanguage().isKindOf(AsciiDocLanguage.INSTANCE)) {
+      if (data != null && data.getViewProvider().getBaseLanguage().isKindOf(AsciiDocLanguage.INSTANCE)) {
         return new AsciiDocToolbarPanel();
       }
     }
