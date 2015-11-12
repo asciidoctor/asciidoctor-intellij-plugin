9d792a1867bcce37439f475167ebf67b48db6492
Michael Krausse <michael.krausse@directbox.com>
2015-11-12 22:20:27 +0100

    ehmkah: refactoring two small methods https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/86

--- src/main/java/org/asciidoc/intellij/actions/asciidoc/FormatAsciiDocAction.java
+++ src/main/java/org/asciidoc/intellij/actions/asciidoc/FormatAsciiDocAction.java
@@ -70,17 +70,30 @@ public abstract class FormatAsciiDocAction extends AsciiDocAction {
     });
   }
 
-  @NotNull
   protected String updateSelectionIntern(String selection, String symbol) {
-    String doubleSymbol = symbol + symbol;
-    if (selection.startsWith(doubleSymbol) && selection.endsWith(doubleSymbol)) {
-      return selection.substring(2, selection.length() - 2);
+    if (containsSymbol(selection, symbol)) {
+      return removeSymbol(selection, symbol);
     }
-    if (selection.startsWith(symbol) && selection.endsWith(symbol)) {
-      return selection.substring(1, selection.length() - 1);
-    }
+    return appendSymbol(selection, symbol);
+  }
 
+  private String appendSymbol(String selection, String symbol) {
+    String doubleSymbol = symbol + symbol;
     return doubleSymbol + selection + doubleSymbol;
   }
+
+  private String removeSymbol(String selection, String symbol) {
+    if (selection.startsWith(symbol + symbol)) {
+      return selection.substring(2, selection.length() - 2);
+    }
+    return selection.substring(1, selection.length() - 1);
+  }
+
+  private boolean containsSymbol(String selection, String symbol) {
+    String doubleSymbol = symbol + symbol;
+    return (selection.startsWith(symbol) && selection.endsWith(symbol)) ||
+        (selection.startsWith(doubleSymbol) && selection.endsWith(doubleSymbol));
+  }
+
 
 }
