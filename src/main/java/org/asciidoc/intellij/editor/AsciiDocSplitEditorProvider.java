678edf457e0000c2450d0e54ac410f29b9638c1b
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-08-12 18:31:01 +0200

    don't throw an AbstractMethodError when closing asciidoc editor and running on IntelliJ 15 #131

--- src/main/java/org/asciidoc/intellij/editor/AsciiDocSplitEditorProvider.java
+++ src/main/java/org/asciidoc/intellij/editor/AsciiDocSplitEditorProvider.java
@@ -3,6 +3,7 @@ package org.asciidoc.intellij.editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorProvider;
+import com.intellij.openapi.util.Disposer;
 import org.asciidoc.intellij.ui.SplitTextEditorProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +18,12 @@ public class AsciiDocSplitEditorProvider extends SplitTextEditorProvider {
       throw new IllegalArgumentException("Main editor should be TextEditor");
     }
     return new AsciiDocSplitEditor(((TextEditor)firstEditor), ((AsciiDocPreviewEditor)secondEditor));
+  }
+
+  @Override
+  public void disposeEditor(@NotNull FileEditor fileEditor) {
+    // default -- needed for IntelliJ IDEA 15 compatibility
+    Disposer.dispose(fileEditor);
   }
 
 }
