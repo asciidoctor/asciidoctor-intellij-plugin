3d8a5290b225e0da5587a7bc27e362bdbd950125
Alexander Schwartz <alexander.schwartz@gmx.net>
2018-05-11 23:18:51 +0200

    duplicating solution of IDEA-173017 to make it work on Windows 10 #125

--- src/main/java/org/asciidoc/intellij/editor/javafx/JFXPanelWrapper.java
+++ src/main/java/org/asciidoc/intellij/editor/javafx/JFXPanelWrapper.java
@@ -1,11 +1,19 @@
 package org.asciidoc.intellij.editor.javafx;
 
+import com.intellij.util.FieldAccessor;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
+import com.sun.javafx.embed.EmbeddedSceneInterface;
+import com.sun.javafx.tk.TKScene;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
 
 import java.awt.*;
 
 public class JFXPanelWrapper extends JFXPanel {
+  private static final FieldAccessor<JFXPanel, Integer> myScaleFactorAccessor = new FieldAccessor<>(JFXPanel.class, "scaleFactor");
+
   public JFXPanelWrapper() {
     Platform.setImplicitExit(false);
   }
@@ -20,5 +28,25 @@ public class JFXPanelWrapper extends JFXPanel {
   @Override
   public Dimension getMinimumSize() {
     return new Dimension(0, 0);
+  }
+
+  @Override
+  public void addNotify() {
+    super.addNotify();
+    if (UIUtil.isJreHiDPIEnabled()) {
+      // JFXPanel is scaled asynchronously after first repaint, what may lead
+      // to showing unscaled content. To work it around, set "scaleFactor" ahead.
+      int scale = Math.round(JBUI.sysScale(this));
+      myScaleFactorAccessor.set(this, scale);
+      Scene scene = getScene();
+      // If scene is null then it will be set later and super.setEmbeddedScene(..) will init its scale properly,
+      // otherwise explicitly set scene scale to match JFXPanel.scaleFactor.
+      if (scene != null) {
+        TKScene tks = scene.impl_getPeer();
+        if (tks instanceof EmbeddedSceneInterface) {
+          ((EmbeddedSceneInterface)tks).setPixelScaleFactor(scale);
+        }
+      }
+    }
   }
 }
