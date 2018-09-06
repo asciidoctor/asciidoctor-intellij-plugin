b7d9053720666b20a0017c1a3afbfff873f1ed92
Alexander Schwartz <alexander.schwartz@gmx.net>
2018-09-06 22:37:04 +0200

    fix source line on bold and other inline formattings in asciidoc 1.5.7, next try

--- src/main/resources/sourceline-treeprocessor.rb
+++ src/main/resources/sourceline-treeprocessor.rb
@@ -8,10 +8,10 @@ class SourceLineTreeProcessor < Extensions::Treeprocessor
     document.find_by.each do |node|
 
       # on each node add the source file information as role (will result in CSS class in HTML)
-      if (node.source_location) then
-        if (node.inline? != true || node.source_location.lineno != 1) then
+      if node.source_location
+        if node.source_location.lineno != 1
           # on AsciiDoc 1.5.7 I've seen source lines of "1" with in inline formatting (i.e. links and bold)
-          # therefore all entries with lineno 1 are ignored
+          # it seems that they have been inherited from the document/parent, therefore all entries with lineno 1 are ignored
           node.attributes['role'] = 'has-source-line data-line-' + (node.source_location.file || 'stdin') + "-#{node.source_location.lineno}"
         end
       end
