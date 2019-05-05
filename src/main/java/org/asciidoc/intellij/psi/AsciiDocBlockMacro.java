dbff05cc2479070b3f0568181930df22c85d503d
Alexander Schwartz <alexander.schwartz@gmx.net>
2019-05-05 12:48:08 +0200

    making linking of documents work for standard includes (#204)

--- src/main/java/org/asciidoc/intellij/psi/AsciiDocBlockMacro.java
+++ src/main/java/org/asciidoc/intellij/psi/AsciiDocBlockMacro.java
@@ -1,9 +1,14 @@
 package org.asciidoc.intellij.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.AbstractElementManipulator;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.util.IncorrectOperationException;
 import org.asciidoc.intellij.lexer.AsciiDocTokenTypes;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +23,7 @@ public class AsciiDocBlockMacro extends AsciiDocBlock {
   @NotNull
   @Override
   public PsiReference[] getReferences() {
-    if (getMacroName().equals("image")) {
+    if (getMacroName().equals("image") || getMacroName().equals("include")) {
       ASTNode bodyNode = getNode().findChildByType(AsciiDocTokenTypes.BLOCK_MACRO_BODY);
       if (bodyNode != null) {
         return new FileReferenceSet(bodyNode.getText(), this, bodyNode.getStartOffset() - getTextRange().getStartOffset(),
@@ -36,4 +41,33 @@ public class AsciiDocBlockMacro extends AsciiDocBlock {
     return StringUtil.trimEnd(idNode.getText(), "::");
 
   }
+
+  public static class Manipulator extends AbstractElementManipulator<AsciiDocBlockMacro> {
+
+    @Override
+    public AsciiDocBlockMacro handleContentChange(@NotNull AsciiDocBlockMacro element,
+                                                  @NotNull TextRange range,
+                                                  String newContent) throws IncorrectOperationException {
+      PsiElement child = element.getFirstChild();
+      while (child != null && child.getNode().getElementType() != AsciiDocTokenTypes.BLOCK_MACRO_BODY) {
+        range = range.shiftRight(-child.getTextLength());
+        child = child.getNextSibling();
+      }
+      if (child instanceof LeafPsiElement) {
+        ((LeafPsiElement) child).replaceWithText(range.replace(child.getText(), newContent));
+      } else {
+        throw new IncorrectOperationException("Bad child");
+      }
+
+      return element;
+    }
+
+    @NotNull
+    @Override
+    public TextRange getRangeInElement(@NotNull AsciiDocBlockMacro element) {
+      final String text = element.findChildByType(AsciiDocTokenTypes.BLOCK_MACRO_BODY).getText();
+      return TextRange.allOf(text);
+    }
+  }
+
 }
