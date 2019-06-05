72382f0732c091fc9962f546e46ccd8fb3ef87a5
Alexander Schwartz <alexander.schwartz@gmx.net>
2019-06-06 00:05:01 +0200

    show includes and images in structure view (#267)

--- src/main/java/org/asciidoc/intellij/structureView/AsciiDocStructureViewElement.java
+++ src/main/java/org/asciidoc/intellij/structureView/AsciiDocStructureViewElement.java
@@ -5,8 +5,12 @@ import com.intellij.ide.structureView.impl.common.PsiTreeElementBase;
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
 import icons.AsciiDocIcons;
 import org.asciidoc.intellij.psi.AsciiDocBlock;
+import org.asciidoc.intellij.psi.AsciiDocBlockMacro;
+import org.asciidoc.intellij.psi.AsciiDocCodeContent;
 import org.asciidoc.intellij.psi.AsciiDocSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -27,8 +31,40 @@ public class AsciiDocStructureViewElement extends PsiTreeElementBase<PsiElement>
   @NotNull
   @Override
   public Collection<StructureViewTreeElement> getChildrenBase() {
-    List<StructureViewTreeElement> result = new ArrayList<StructureViewTreeElement>();
+    List<StructureViewTreeElement> result = new ArrayList<>();
+    if (getElement() == null) {
+      return result;
+    }
     for (PsiElement childElement : getElement().getChildren()) {
+      // a block macro might contain references to other files (for example an include or an image)
+      if (childElement instanceof AsciiDocBlockMacro) {
+        PsiReference[] references = childElement.getReferences();
+        if (references.length > 0) {
+          if (!getPresentableElementText(references[0].resolve()).isEmpty()) {
+            result.add(new AsciiDocStructureViewElement(references[0].resolve()));
+            continue;
+          }
+        }
+      }
+      if (childElement instanceof AsciiDocCodeContent) {
+        // the code content might contain includes
+        AsciiDocBlockMacro[] blocks = PsiTreeUtil.getChildrenOfType(childElement, AsciiDocBlockMacro.class);
+        if (blocks != null) {
+          boolean found = false;
+          for (AsciiDocBlockMacro block : blocks) {
+            PsiReference[] references = block.getReferences();
+            if (references.length > 0) {
+              if (!getPresentableElementText(references[0].resolve()).isEmpty()) {
+                result.add(new AsciiDocStructureViewElement(references[0].resolve()));
+                found = true;
+              }
+            }
+          }
+          if (found) {
+            continue;
+          }
+        }
+      }
       if (!getPresentableElementText(childElement).isEmpty()) {
         result.add(new AsciiDocStructureViewElement(childElement));
       }
@@ -50,15 +86,19 @@ public class AsciiDocStructureViewElement extends PsiTreeElementBase<PsiElement>
       @Nullable
       @Override
       public String getLocationString() {
-        getElement().getContainingFile().getName();
         return null;
       }
 
       @Nullable
       @Override
       public Icon getIcon(boolean unused) {
-        return AsciiDocStructureViewElement.this.getElement() instanceof AsciiDocSection ?
-          AsciiDocIcons.Asciidoc_Icon : null;
+        if (AsciiDocStructureViewElement.this.getElement() instanceof AsciiDocSection) {
+          return AsciiDocIcons.Asciidoc_Icon;
+        }
+        if (AsciiDocStructureViewElement.this.getElement() instanceof PsiFile) {
+          return AsciiDocStructureViewElement.this.getElement().getIcon(0);
+        }
+        return null;
       }
     };
   }
@@ -71,21 +111,22 @@ public class AsciiDocStructureViewElement extends PsiTreeElementBase<PsiElement>
 
   private static String getPresentableElementText(PsiElement element) {
     if (element instanceof AsciiDocSection) {
-      return ((AsciiDocSection)element).getTitle();
+      return ((AsciiDocSection) element).getTitle();
     }
     if (element instanceof PsiFile) {
-      return ((PsiFile)element).getName();
+      return ((PsiFile) element).getName();
     }
     if (element instanceof AsciiDocBlock) {
-      AsciiDocBlock block = (AsciiDocBlock)element;
+      AsciiDocBlock block = (AsciiDocBlock) element;
       String title = block.getTitle();
-      if (title != null) {
-        String style = block.getStyle();
-        if (style != null) {
-          return "[" + style + "] " + title;
-        }
-        return title;
+      if (title == null) {
+        title = "(Block)";
       }
+      String style = block.getStyle();
+      if (style != null) {
+        return "[" + style + "] " + title;
+      }
+      return title;
     }
     return "";
   }
