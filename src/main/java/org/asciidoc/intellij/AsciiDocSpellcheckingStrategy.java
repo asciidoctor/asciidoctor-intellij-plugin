5d8f07b992e8ee17ba04810755a120b313f071d5
Alexander Schwartz <alexander.schwartz@gmx.net>
2019-06-06 18:59:42 +0200

    refactoring for token set

--- src/main/java/org/asciidoc/intellij/AsciiDocSpellcheckingStrategy.java
+++ src/main/java/org/asciidoc/intellij/AsciiDocSpellcheckingStrategy.java
@@ -2,6 +2,7 @@ package org.asciidoc.intellij;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import org.asciidoc.intellij.lexer.AsciiDocTokenTypes;
@@ -11,20 +12,22 @@ import org.jetbrains.annotations.NotNull;
  * @author yole
  */
 public class AsciiDocSpellcheckingStrategy extends SpellcheckingStrategy {
+  public static TokenSet TEXT_TOKENS = TokenSet.create(AsciiDocTokenTypes.HEADING,
+    AsciiDocTokenTypes.TEXT,
+    AsciiDocTokenTypes.BOLD,
+    AsciiDocTokenTypes.ITALIC,
+    AsciiDocTokenTypes.MONO,
+    AsciiDocTokenTypes.BOLDITALIC,
+    AsciiDocTokenTypes.MONOBOLD,
+    AsciiDocTokenTypes.MONOITALIC,
+    AsciiDocTokenTypes.TITLE,
+    AsciiDocTokenTypes.MONOBOLDITALIC);
+
   @NotNull
   @Override
   public Tokenizer getTokenizer(PsiElement element) {
     IElementType elementType = element.getNode().getElementType();
-    if (elementType == AsciiDocTokenTypes.HEADING
-      || elementType == AsciiDocTokenTypes.TEXT
-      || elementType == AsciiDocTokenTypes.BOLD
-      || elementType == AsciiDocTokenTypes.ITALIC
-      || elementType == AsciiDocTokenTypes.MONO
-      || elementType == AsciiDocTokenTypes.BOLDITALIC
-      || elementType == AsciiDocTokenTypes.MONOBOLD
-      || elementType == AsciiDocTokenTypes.MONOITALIC
-      || elementType == AsciiDocTokenTypes.TITLE
-      || elementType == AsciiDocTokenTypes.MONOBOLDITALIC) {
+    if (TEXT_TOKENS.contains(elementType)) {
       return TEXT_TOKENIZER;
     }
     return EMPTY_TOKENIZER;
