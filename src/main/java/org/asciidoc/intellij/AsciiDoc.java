d68364a25b38b4f6fe69f5543ce8c2d0b171cf5c
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-07-05 11:02:52 +0200

    support for '.adoc' extension of .asciidoctorconfig file #293 (#294)
    * support for '.adoc' extension of .asciidoctorconfig file #293

--- src/main/java/org/asciidoc/intellij/AsciiDoc.java
+++ src/main/java/org/asciidoc/intellij/AsciiDoc.java
@@ -253,16 +253,18 @@ public class AsciiDoc {
     VirtualFile folder = currentFile.getParent();
     if (folder != null) {
       while (true) {
-        VirtualFile configFile = folder.findChild(".asciidoctorconfig");
-        if (configFile != null &&
-          !currentFile.equals(configFile)) {
-          Document config = FileDocumentManager.getInstance().getDocument(configFile);
-          if (config != null) {
-            // prepend the new config, followed by two newlines to avoid sticking-together content
-            tempContent.insert(0, "\n\n");
-            tempContent.insert(0, config.getText());
-            // prepend the location of the config file
-            tempContent.insert(0, ":asciidoctorconfigdir: " + folder.getCanonicalPath() + "\n\n");
+        for (String configName : new String[]{".asciidoctorconfig", ".asciidoctorconfig.adoc"}) {
+          VirtualFile configFile = folder.findChild(configName);
+          if (configFile != null &&
+            !currentFile.equals(configFile)) {
+            Document config = FileDocumentManager.getInstance().getDocument(configFile);
+            if (config != null) {
+              // prepend the new config, followed by two newlines to avoid sticking-together content
+              tempContent.insert(0, "\n\n");
+              tempContent.insert(0, config.getText());
+              // prepend the location of the config file
+              tempContent.insert(0, ":asciidoctorconfigdir: " + folder.getCanonicalPath() + "\n\n");
+            }
           }
         }
         if (folder.getPath().equals(project.getBasePath())) {
