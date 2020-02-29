0157a6f7cf813c726a9efbc2027c535c469bbba6
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-02-29 13:43:24 +0100

    adding folding for predefined attributes for character replacements

--- src/main/java/org/asciidoc/intellij/folding/AsciiDocFoldingBuilder.java
+++ src/main/java/org/asciidoc/intellij/folding/AsciiDocFoldingBuilder.java
@@ -9,14 +9,50 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import org.asciidoc.intellij.inspections.AsciiDocVisitor;
+import org.asciidoc.intellij.psi.AsciiDocAttributeReference;
 import org.asciidoc.intellij.psi.AsciiDocBlock;
 import org.asciidoc.intellij.psi.AsciiDocSection;
 import org.asciidoc.intellij.psi.AsciiDocSelfDescribe;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AsciiDocFoldingBuilder extends CustomFoldingBuilder implements DumbAware {
+  private static final Map<String, String> COLLAPSABLE_ATTRIBUTES = new HashMap<>();
+
+  // https://asciidoctor.org/docs/user-manual/#charref-attributes
+  // only fold those pre-defined attributes that have visible characters
+  // that is: leaving out blank, zwsp and wj
+  static {
+    COLLAPSABLE_ATTRIBUTES.put("sp", " ");
+    COLLAPSABLE_ATTRIBUTES.put("nbsp", " ");
+    COLLAPSABLE_ATTRIBUTES.put("apos", "'");
+    COLLAPSABLE_ATTRIBUTES.put("quot", "\"");
+    COLLAPSABLE_ATTRIBUTES.put("lsquo", "‘");
+    COLLAPSABLE_ATTRIBUTES.put("rsquo", "’");
+    COLLAPSABLE_ATTRIBUTES.put("ldquo", "“");
+    COLLAPSABLE_ATTRIBUTES.put("rdquo", "”");
+    COLLAPSABLE_ATTRIBUTES.put("deg", "°");
+    COLLAPSABLE_ATTRIBUTES.put("plus", "+");
+    COLLAPSABLE_ATTRIBUTES.put("brvbar", "¦");
+    COLLAPSABLE_ATTRIBUTES.put("vbar", "|");
+    COLLAPSABLE_ATTRIBUTES.put("amp", "&");
+    COLLAPSABLE_ATTRIBUTES.put("lt", "<");
+    COLLAPSABLE_ATTRIBUTES.put("gt", ">");
+    COLLAPSABLE_ATTRIBUTES.put("startsb", "[");
+    COLLAPSABLE_ATTRIBUTES.put("endsb", "]");
+    COLLAPSABLE_ATTRIBUTES.put("caret", "^");
+    COLLAPSABLE_ATTRIBUTES.put("asterisk", "*");
+    COLLAPSABLE_ATTRIBUTES.put("tilde", "~");
+    COLLAPSABLE_ATTRIBUTES.put("backslash", "\\");
+    COLLAPSABLE_ATTRIBUTES.put("backtick", "`");
+    COLLAPSABLE_ATTRIBUTES.put("two-colons", "::");
+    COLLAPSABLE_ATTRIBUTES.put("two-semicolons", ";;");
+    COLLAPSABLE_ATTRIBUTES.put("cpp", "C++");
+  }
+
   @Override
   protected void buildLanguageFoldRegions(@NotNull List<FoldingDescriptor> descriptors,
                                           @NotNull PsiElement root,
@@ -26,6 +62,14 @@ public class AsciiDocFoldingBuilder extends CustomFoldingBuilder implements Dumb
 
       @Override
       public void visitElement(PsiElement element) {
+        if (element instanceof AsciiDocAttributeReference) {
+          String text = element.getText();
+          if (text.startsWith("{") && text.endsWith("}")) {
+            if (COLLAPSABLE_ATTRIBUTES.containsKey(text.substring(1, text.length() - 1))) {
+              addDescriptors(element);
+            }
+          }
+        }
         super.visitElement(element);
         element.acceptChildren(this);
       }
@@ -66,6 +110,8 @@ public class AsciiDocFoldingBuilder extends CustomFoldingBuilder implements Dumb
                                      @NotNull Document document) {
     if (document.getLineNumber(range.getStartOffset()) != document.getLineNumber(range.getEndOffset() - 1)) {
       descriptors.add(new FoldingDescriptor(element, range));
+    } else if (element instanceof AsciiDocAttributeReference) {
+      descriptors.add(new FoldingDescriptor(element, range));
     }
   }
 
@@ -76,6 +122,16 @@ public class AsciiDocFoldingBuilder extends CustomFoldingBuilder implements Dumb
       title = ((AsciiDocSelfDescribe) node.getPsi()).getFoldedSummary();
       title = StringUtil.shortenTextWithEllipsis(title, 50, 5);
       title += " ...";
+    } else if (node.getPsi() instanceof AsciiDocAttributeReference) {
+      String text = node.getText();
+      if (text.startsWith("{") && text.endsWith("}")) {
+        title = COLLAPSABLE_ATTRIBUTES.get(text.substring(1, text.length() - 1));
+        if (title == null) {
+          title = text;
+        }
+      } else {
+        title = text;
+      }
     } else {
       title = StringUtil.shortenTextWithEllipsis(node.getText(), 50, 5);
     }
