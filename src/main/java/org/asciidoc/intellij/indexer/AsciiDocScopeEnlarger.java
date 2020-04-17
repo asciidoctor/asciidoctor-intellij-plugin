600ba6fdbe47347454401b717a25b7d9c2ebadbf
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-04-17 20:42:02 +0200

    enlarge search scope for finding references to all AsciiDoc documents in project independent of project's module dependencies

--- src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
+++ src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
@@ -4,7 +4,6 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.UseScopeEnlarger;
-import org.asciidoc.intellij.AsciiDocLanguage;
 import org.asciidoc.intellij.file.AsciiDocFileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,9 +12,7 @@ public class AsciiDocScopeEnlarger extends UseScopeEnlarger {
   @Nullable
   @Override
   public SearchScope getAdditionalUseScope(@NotNull PsiElement element) {
-    if (element.getLanguage() == AsciiDocLanguage.INSTANCE) {
-      return GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.projectScope(element.getProject()), AsciiDocFileType.INSTANCE);
-    }
-    return null;
+    // any element can be referenced from an AsciiDoc element within the same project (directory, file, image, or an ID in an AsciiDoc file)
+    return GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.projectScope(element.getProject()), AsciiDocFileType.INSTANCE);
   }
 }
