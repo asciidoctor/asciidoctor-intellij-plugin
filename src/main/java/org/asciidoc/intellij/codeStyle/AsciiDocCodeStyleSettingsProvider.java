7a4335d01fb128d71f696e2135665cede86e67bc
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-05-31 12:00:09 +0200

    updated deprecated methods in plugin's Java code

--- src/main/java/org/asciidoc/intellij/codeStyle/AsciiDocCodeStyleSettingsProvider.java
+++ src/main/java/org/asciidoc/intellij/codeStyle/AsciiDocCodeStyleSettingsProvider.java
@@ -4,7 +4,7 @@ import com.intellij.application.options.CodeStyleAbstractConfigurable;
 import com.intellij.application.options.CodeStyleAbstractPanel;
 import com.intellij.application.options.TabbedLanguageCodeStylePanel;
 import com.intellij.lang.Language;
-import com.intellij.openapi.options.Configurable;
+import com.intellij.psi.codeStyle.CodeStyleConfigurable;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
@@ -27,7 +27,7 @@ public class AsciiDocCodeStyleSettingsProvider extends CodeStyleSettingsProvider
 
   @NotNull
   @Override
-  public Configurable createSettingsPage(CodeStyleSettings settings, CodeStyleSettings modelSettings) {
+  public CodeStyleConfigurable createConfigurable(@NotNull CodeStyleSettings settings, @NotNull CodeStyleSettings modelSettings) {
     return new CodeStyleAbstractConfigurable(settings, modelSettings, this.getConfigurableDisplayName()) {
       @Override
       protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings) {
