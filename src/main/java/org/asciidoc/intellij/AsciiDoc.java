f5178b2064883583383fdc462e125aac4ccdbaa9
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-06-03 23:42:02 +0200

    handling null values in Antora module descriptor, parsing special values `~` and `@` (#504)

--- src/main/java/org/asciidoc/intellij/AsciiDoc.java
+++ src/main/java/org/asciidoc/intellij/AsciiDoc.java
@@ -758,7 +758,6 @@ public class AsciiDoc {
         ApplicationManager.getApplication().runReadAction(() -> {
           Document document = FileDocumentManager.getInstance().getDocument(antoraFile);
           if (document != null) {
-            Yaml yaml = new Yaml();
             try {
               Map<String, Object> antora = readAntoraYaml(antoraFile);
               mapAttribute(result, antora, "name", "page-component-name");
@@ -771,11 +770,35 @@ public class AsciiDoc {
                 @SuppressWarnings("rawtypes") Object attributes = ((Map) asciidoc).get("attributes");
                 if (attributes instanceof Map) {
                   @SuppressWarnings("unchecked") Map<String, Object> map = (Map<String, Object>) attributes;
-                  map.forEach((k, v) -> result.put(k, v.toString()));
+                  map.forEach((k, v) -> {
+                    String vs;
+                    if (v == null) {
+                      // null -> not allowed in YAML file as attribute value
+                      Notification notification = AsciiDocPreviewEditor.NOTIFICATION_GROUP.createNotification("AsciiDoc attribute '" + k + "' is null in " + antoraFile.getCanonicalPath(),
+                        "Will be treated as unset. Use either false to explicitly unset, or set by providing for example an empty string ''",
+                        NotificationType.ERROR, null);
+                      notification.setImportant(true);
+                      Notifications.Bus.notify(notification);
+                      vs = null;
+                    } else if (v instanceof Boolean && !(Boolean) v) {
+                      // false -> soft unset
+                      vs = null;
+                    } else if (v instanceof String && v.equals("~")) {
+                      // "~" -> hard unset
+                      vs = null;
+                    } else {
+                      vs = v.toString();
+                      if (vs.endsWith("@")) {
+                        // "...@" -> soft set
+                        vs = vs.substring(0, vs.length() - 1);
+                      }
+                    }
+                    result.put(k, vs);
+                  });
                 }
               }
             } catch (YAMLException ignored) {
-              // continue without detailed Antora informatoin
+              // continue without detailed Antora information
             }
           }
         });
