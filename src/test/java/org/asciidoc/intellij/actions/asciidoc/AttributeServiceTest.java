361970588c8bee129862a026fd24a4635d01b57e
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-10-18 13:37:47 +0200

    reviewed test: prefixed test methods with "should" to describe expected outcome. Reversed arguments for assert() as expected value is first argument

--- src/test/java/org/asciidoc/intellij/actions/asciidoc/AttributeServiceTest.java
+++ src/test/java/org/asciidoc/intellij/actions/asciidoc/AttributeServiceTest.java
@@ -18,39 +18,39 @@ public class AttributeServiceTest {
   }
 
   @Test
-  public void mapsAttributesToCommaSeparatedLabelValueString() {
+  public void shouldMapAttributesToCommaSeparatedLabelValueString() {
     final Pair<Optional<?>, Optional<String>> attribute1 = new Pair<>(Optional.of("1"), Optional.of("a1"));
     final Pair<Optional<?>, Optional<String>> attribute2 = new Pair<>(Optional.of(2), Optional.of("a2"));
     final Pair<Optional<?>, Optional<String>> attribute3 = new Pair<>(Optional.of(true), Optional.of("a3"));
 
     final String attributes = service.toAttributeString(attribute1, attribute2, attribute3);
 
-    assertEquals(attributes, "a1=1,a2=2,a3=true");
+    assertEquals("a1=1,a2=2,a3=true", attributes);
   }
 
   @Test
-  public void mapsAttributesWithOptionalLabelCorrectly() {
+  public void shouldMapAttributesWithEmptyAndNonEmptyOptionalLabelCorrectly() {
     final Pair<Optional<?>, Optional<String>> attribute1 = new Pair<>(Optional.of("source"), Optional.empty());
     final Pair<Optional<?>, Optional<String>> attribute2 = new Pair<>(Optional.of("java"), Optional.empty());
     final Pair<Optional<?>, Optional<String>> attribute3 = new Pair<>(Optional.of("\"Example 1\""), Optional.of("title"));
 
     final String attributes = service.toAttributeString(attribute1, attribute2, attribute3);
 
-    assertEquals(attributes, "source,java,title=\"Example 1\"");
+    assertEquals("source,java,title=\"Example 1\"", attributes);
   }
 
   @Test
-  public void filtersEmptyOptionalValues() {
+  public void shouldFiltersEmptyOptionalValues() {
     final Pair<Optional<?>, Optional<String>> attribute = new Pair<>(Optional.of(true), Optional.of("attribute"));
     final Pair<Optional<?>, Optional<String>> empty = new Pair<>(Optional.empty(), Optional.of("empty"));
 
     final String attributes = service.toAttributeString(attribute, empty);
 
-    assertEquals(attributes, "attribute=true");
+    assertEquals("attribute=true", attributes);
   }
 
   @Test
-  public void returnsAnEmptyStringIfInvokedWithoutArgs() {
-    assertEquals(service.toAttributeString(), "");
+  public void shouldReturnAnEmptyStringIfInvokedWithoutArgs() {
+    assertEquals("", service.toAttributeString());
   }
 }
