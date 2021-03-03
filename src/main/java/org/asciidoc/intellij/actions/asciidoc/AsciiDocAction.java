489413b913a13f7c26b8000ccafbbaf521e815f7
Alexander Schwartz <alexander.schwartz@gmx.net>
2021-03-03 08:53:54 +0100

    avoid exception when processing events for already disposed projects (#683)

--- src/main/java/org/asciidoc/intellij/actions/asciidoc/AsciiDocAction.java
+++ src/main/java/org/asciidoc/intellij/actions/asciidoc/AsciiDocAction.java
@@ -5,8 +5,8 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
-import com.intellij.serviceContainer.AlreadyDisposedException;
 import org.asciidoc.intellij.AsciiDocLanguage;
 import org.asciidoc.intellij.file.AsciiDocFileType;
 import org.jetbrains.annotations.NotNull;
@@ -21,25 +21,26 @@ public abstract class AsciiDocAction extends AnAction implements DumbAware, Ligh
 
   @Override
   public void update(@NotNull AnActionEvent event) {
-    try {
-      PsiFile file = event.getData(LangDataKeys.PSI_FILE);
-      boolean enabled = false;
-      if (file != null) {
-        if (file.getLanguage() == AsciiDocLanguage.INSTANCE) {
-          enabled = true;
-        } else {
-          for (String ext : AsciiDocFileType.DEFAULT_ASSOCIATED_EXTENSIONS) {
-            if (file.getName().endsWith("." + ext)) {
-              enabled = true;
-              break;
-            }
+    PsiFile file = event.getData(LangDataKeys.PSI_FILE);
+    Project project = event.getProject();
+    if (project != null && project.isDisposed()) {
+      // otherwise classes like SingleRootFileViewProvider.createFile() might log an exception
+      return;
+    }
+    boolean enabled = false;
+    if (file != null) {
+      if (file.getLanguage() == AsciiDocLanguage.INSTANCE) {
+        enabled = true;
+      } else {
+        for (String ext : AsciiDocFileType.DEFAULT_ASSOCIATED_EXTENSIONS) {
+          if (file.getName().endsWith("." + ext)) {
+            enabled = true;
+            break;
           }
         }
       }
-      event.getPresentation().setEnabledAndVisible(enabled);
-    } catch (AlreadyDisposedException ex) {
-      // ignored - module has already been disposed
     }
+    event.getPresentation().setEnabledAndVisible(enabled);
   }
 
 }
