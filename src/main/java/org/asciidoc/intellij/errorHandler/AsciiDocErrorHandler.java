d572b01fc7561bb4e89d5607abb0a8d3a4dc3dc9
Alexander Schwartz <alexander.schwartz@gmx.net>
2021-05-12 15:26:22 +0200

    unwrap the PluginException to reduce the length of the thread dump

--- src/main/java/org/asciidoc/intellij/errorHandler/AsciiDocErrorHandler.java
+++ src/main/java/org/asciidoc/intellij/errorHandler/AsciiDocErrorHandler.java
@@ -1,6 +1,7 @@
 package org.asciidoc.intellij.errorHandler;
 
 import com.intellij.diagnostic.AbstractMessage;
+import com.intellij.diagnostic.PluginException;
 import com.intellij.openapi.diagnostic.Attachment;
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
@@ -33,6 +34,10 @@ public class AsciiDocErrorHandler extends ErrorReportSubmitter {
       if (event.getData() instanceof AbstractMessage) {
         throwable = ((AbstractMessage) event.getData()).getThrowable();
         attachments = ((AbstractMessage) event.getData()).getIncludedAttachments();
+      }
+      if (throwable instanceof PluginException && throwable.getCause() != null) {
+        // unwrap PluginManagerCore.createPluginException
+        throwable = throwable.getCause();
       }
       SentryErrorReporter.submitErrorReport(throwable, attachments, additionalInfo, consumer);
     }
