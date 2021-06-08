9937b55f59f48b9b8f742cf66e7ec66e1eb91a84
Alexander Schwartz <alexander.schwartz@gmx.net>
2021-06-08 14:47:28 +0200

    cleanup code after drop of 2020.2 compatibility

--- src/main/java/org/asciidoc/intellij/formatting/AsciiDocFormattingModelBuilder.java
+++ src/main/java/org/asciidoc/intellij/formatting/AsciiDocFormattingModelBuilder.java
@@ -1,12 +1,12 @@
 package org.asciidoc.intellij.formatting;
 
+import com.intellij.formatting.FormattingContext;
 import com.intellij.formatting.FormattingModel;
 import com.intellij.formatting.FormattingModelBuilder;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.FormattingDocumentModelImpl;
 import com.intellij.psi.formatter.PsiBasedFormattingModel;
 import com.intellij.psi.impl.source.tree.TreeElement;
@@ -17,10 +17,11 @@ import org.jetbrains.annotations.Nullable;
 public class AsciiDocFormattingModelBuilder implements FormattingModelBuilder {
   @NotNull
   @Override
-  public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
+  public FormattingModel createModel(FormattingContext context) {
+    final PsiElement element = context.getPsiElement();
     final ASTNode root = TreeUtil.getFileElement((TreeElement) element.getNode());
     final FormattingDocumentModelImpl documentModel = FormattingDocumentModelImpl.createOn(element.getContainingFile());
-    return new PsiBasedFormattingModel(element.getContainingFile(), new AsciiDocBlock(root, settings), documentModel);
+    return new PsiBasedFormattingModel(element.getContainingFile(), new AsciiDocBlock(root, context.getCodeStyleSettings()), documentModel);
   }
 
   @Nullable
