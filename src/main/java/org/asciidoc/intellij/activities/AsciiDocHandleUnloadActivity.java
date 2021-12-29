79376a6bb0a722bc453d695bf37fc302303fb91c
Alexander Schwartz <alexander.schwartz@gmx.net>
2021-12-29 20:52:36 +0100

    created upstream issue

--- src/main/java/org/asciidoc/intellij/activities/AsciiDocHandleUnloadActivity.java
+++ src/main/java/org/asciidoc/intellij/activities/AsciiDocHandleUnloadActivity.java
@@ -50,7 +50,7 @@ public class AsciiDocHandleUnloadActivity implements StartupActivity, DumbAware,
               if (Objects.equals(pluginDescriptor.getPluginId().getIdString(), AsciiDocPlugin.PLUGIN_ID)) {
                 LOG.info("checkUnloadPlugin");
                 // https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/512
-                // another reason: on windows even after unloading JAR file of the plugin still be locked and can't be deleted, making uninstall impossible
+                // another reason: on Windows even after unloading JAR file of the plugin still be locked and can't be deleted, making uninstall impossible
                 // https://youtrack.jetbrains.com/issue/IDEA-244471
                 // Update: IDEA-244471 might not be relevant here as the plugin will have the version number in the JAR file, therefore a change file will have a new name
 
@@ -59,6 +59,7 @@ public class AsciiDocHandleUnloadActivity implements StartupActivity, DumbAware,
                 throw new CannotUnloadPluginException("unloading mechanism is not safe, incomplete unloading might lead to strange exceptions");
                 // }
                 // found that "IdeScriptEngineManagerImpl" will hold a reference to "org.jruby.embed.jsr223.JRubyEngineFactory"
+                // https://youtrack.jetbrains.com/issue/IDEA-285933
                 // AsciiDoc.checkUnloadPlugin();
               }
             }
