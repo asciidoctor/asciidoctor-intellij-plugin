6a33f4cd80e35be83caf9893c7a64ab064d7b95d
Alexander Schwartz <alexander.schwartz@gmx.net>
2022-02-06 13:19:03 +0100

    prevent false positive grammar messages after unknown texts (#980)

---
+++ src/main/java/org/asciidoc/intellij/grazie/AsciiDocGrazieProblemFilter.java
@@ -0,0 +1,34 @@
+package org.asciidoc.intellij.grazie;
+
+import com.intellij.grazie.text.ProblemFilter;
+import com.intellij.grazie.text.TextProblem;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
+import org.jetbrains.annotations.NotNull;
+
+public class AsciiDocGrazieProblemFilter extends ProblemFilter {
+
+  private final AsciiDocLanguageSupport languageSupport = new AsciiDocLanguageSupport();
+
+  @Override
+  public boolean shouldIgnore(@NotNull TextProblem problem) {
+    if (problem.getHighlightRanges().size() == 1) {
+      // start with the character just before the problem
+      int start = problem.getHighlightRanges().get(0).getStartOffset() - 1;
+      while (start > 0 && problem.getText().charAt(start) == ' ') {
+        // If we're at a whitespace, and the previous element is UNKNOWN, ignore this problem message.
+        // This handles problems when parsing a text like "Foo bar. <<id>> should be modelled."
+        // would ask for "should" to be capitalized as it is at the start of a sentence.
+        PsiElement element = problem.getText().findPsiElementAt(start);
+        if (element instanceof PsiWhiteSpace && element.getPrevSibling() != null) {
+          if (languageSupport.getElementBehavior(element.getParent(), element) == AsciiDocLanguageSupport.Behavior.UNKNOWN) {
+            return true;
+          }
+        }
+        // repeat while we're at a whitespace
+        start--;
+      }
+    }
+    return false;
+  }
+}
