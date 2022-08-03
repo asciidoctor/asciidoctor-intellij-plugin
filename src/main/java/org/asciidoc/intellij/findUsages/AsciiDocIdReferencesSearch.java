073d2799bba4d3a6ca821b823b877f2746fda9a0
Alexander Schwartz <alexander.schwartz@gmx.net>
2022-08-03 17:34:20 +0200

    Rework conditions to avoid read lock problems (#1121)
    This avoids "Read access is allowed from inside read-action (or EDT) only" on isValid()

--- src/main/java/org/asciidoc/intellij/findUsages/AsciiDocIdReferencesSearch.java
+++ src/main/java/org/asciidoc/intellij/findUsages/AsciiDocIdReferencesSearch.java
@@ -52,152 +52,156 @@ public class AsciiDocIdReferencesSearch extends QueryExecutorBase<PsiReference, 
   @Override
   public void processQuery(@NotNull ReferencesSearch.SearchParameters p, @NotNull Processor<? super PsiReference> consumer) {
     final PsiElement element = p.getElementToSearch();
-    if (!element.isValid()) {
+    if (!(element instanceof AsciiDocNamedElement) && !(element instanceof PsiDirectory)) {
+      return;
+    }
+    AsciiDocProcessUtil.runInReadActionWithWriteActionPriority(() -> {
+      if (!element.isValid()) {
+        return;
+      }
+
+      // use scope determined by user here, as effective search scope would return the module and its dependants
+      SearchScope scope = p.getScopeDeterminedByUser();
+
+      if (element instanceof AsciiDocNamedElement) {
+        String name = ((AsciiDocNamedElement) element).getName();
+        if (name != null && name.length() > 0) {
+          search(consumer, element, name, scope);
+        }
+      } else //noinspection ConstantConditions
+        if (element instanceof PsiDirectory) {
+        String name = ((PsiDirectory) element).getName();
+        if (name.endsWith("s") && name.length() > 1) {
+          // it might be a partials, attachments, etc. directory; search for the family and attribute
+          VirtualFile antoraModuleDir = AsciiDocUtil.findAntoraModuleDir(element);
+          if (antoraModuleDir != null) {
+            // partials -> partial$
+            search(consumer, element, name.substring(0, name.length() - 1), scope);
+            // partials -> partialsdir
+            search(consumer, element, name + "dir", scope);
+          }
+        }
+      }
+    });
+  }
+
+  private void search(@NotNull Processor<? super PsiReference> consumer, PsiElement element, String name, SearchScope scope) {
+    DumbService myDumbService = DumbService.getInstance(element.getProject());
+    PsiFile[] files;
+    boolean localSearch = false;
+    if (scope instanceof GlobalSearchScope) {
+      // always restrict to project files, never search libraries
+      scope = ((GlobalSearchScope) scope).intersectWith(GlobalSearchScope.projectScope(element.getProject()));
+      if (!(element instanceof AsciiDocTagDeclaration)) {
+        scope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope) scope, AsciiDocFileType.INSTANCE);
+      }
+      // when the user searches all references
+      String nameToSearch = name;
+      if (element instanceof AsciiDocBlockId) {
+        // AsciiDoc block IDs contain attributes, these won't be in the index as words, therefore search only for the start part (or the attribute name)
+        int attributeStart = name.indexOf('{');
+        int attributeEnd = name.indexOf('}', attributeStart);
+        if (attributeStart > 0) {
+          nameToSearch = name.substring(0, attributeStart);
+        } else if (attributeEnd - (attributeStart + 1) > 0) {
+          nameToSearch = name.substring(attributeStart + 1, attributeEnd);
+        }
+      }
+      String nameFinal = nameToSearch;
+
+      GlobalSearchScope finalScope = (GlobalSearchScope) scope;
+      files = myDumbService.runReadActionInSmartMode(() -> {
+        short context = UsageSearchContext.IN_CODE;
+        if (element instanceof AsciiDocTagDeclaration) {
+          context = UsageSearchContext.ANY;
+        }
+        return CacheManager.getInstance(element.getProject())
+          .getFilesWithWord(nameFinal, context, finalScope, false);
+        }
+      );
+
+      if (element instanceof AsciiDocAttributeDeclarationName) {
+        Collection<AsciiDocAttributeDeclaration> asciiDocAttributeDeclarations = AsciiDocAttributeDeclarationKeyIndex.getInstance().get(name, element.getProject(),
+          // searching also the libraries is generally not useful; therefore restrict to project scope
+          ((GlobalSearchScope) scope).intersectWith(new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType())
+        );
+        for (AsciiDocAttributeDeclaration attribute : asciiDocAttributeDeclarations) {
+          AsciiDocAttributeDeclarationName child = attribute.getAttributeDeclarationName();
+          if (child != null) {
+            consumePsiElementForRename(consumer, child);
+          }
+        }
+      }
+
+      if (files.length == 0) {
+        return;
+      }
+    } else if (scope instanceof LocalSearchScope) {
+      localSearch = true;
+      // when the IDE highlights references of the current file in the editor
+      LocalSearchScope localSearchScope = (LocalSearchScope) scope;
+      if (localSearchScope.getScope().length == 1 && localSearchScope.getScope()[0] instanceof PsiFile) {
+        files = new PsiFile[]{(PsiFile) localSearchScope.getScope()[0]};
+      } else {
+        return;
+      }
+    } else {
       return;
     }
 
-    // use scope determined by user here, as effective search scope would return the module and its dependants
-    SearchScope scope = p.getScopeDeterminedByUser();
-
-    if (element instanceof AsciiDocNamedElement) {
-      String name = ((AsciiDocNamedElement) element).getName();
-      if (name != null && name.length() > 0) {
-          search(consumer, element, name, scope);
-      }
-    } else if (element instanceof PsiDirectory) {
-      String name = ((PsiDirectory) element).getName();
-      if (name.endsWith("s") && name.length() > 1) {
-        // it might be a partials, attachments, etc. directory; search for the family and attribute
-        VirtualFile antoraModuleDir = AsciiDocUtil.findAntoraModuleDir(element);
-        if (antoraModuleDir != null) {
-          // partials -> partial$
-          search(consumer, element, name.substring(0, name.length() - 1), scope);
-          // partials -> partialsdir
-          search(consumer, element, name + "dir", scope);
-        }
-      }
+    // default, like for: AsciiDocAttributeDeclarationName
+    boolean caseSensitive = false;
+    if (element instanceof AsciiDocBlockId || element instanceof AsciiDocTagDeclaration) {
+      caseSensitive = true;
     }
-  }
-
-  private void search(@NotNull Processor<? super PsiReference> consumer, PsiElement element, String name, SearchScope scopeParam) {
-    AsciiDocProcessUtil.runInReadActionWithWriteActionPriority(() -> {
-      SearchScope scope = scopeParam;
-      DumbService myDumbService = DumbService.getInstance(element.getProject());
-      PsiFile[] files;
-      boolean localSearch = false;
-      if (scope instanceof GlobalSearchScope) {
-        // always restrict to project files, never search libraries
-        scope = ((GlobalSearchScope) scope).intersectWith(GlobalSearchScope.projectScope(element.getProject()));
-        if (!(element instanceof AsciiDocTagDeclaration)) {
-          scope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope) scope, AsciiDocFileType.INSTANCE);
+    final StringSearcher asciidocSearcher = new StringSearcher(name, caseSensitive, true, false);
+    final StringSearcher tagdeclarationSearcher = new StringSearcher("::" + name + "[]", caseSensitive, true, false);
+    @Nullable ProgressIndicator pi = ProgressManager.getInstance().getProgressIndicator();
+    if (pi != null) {
+      pi.setText("Searching text in " + files.length + " files");
+      pi.setIndeterminate(false);
+    }
+    for (int i = 0; i < files.length; i++) {
+      PsiFile psiFile = files[i];
+      if (pi != null) {
+        pi.setFraction((double) i / files.length);
+        VirtualFile vf = psiFile.getVirtualFile();
+        if (vf != null) {
+          pi.setText2(vf.getPresentableName());
+        } else {
+          pi.setText2(null);
         }
-        // when the user searches all references
-        String nameToSearch = name;
-        if (element instanceof AsciiDocBlockId) {
-          // AsciiDoc block IDs contain attributes, these won't be in the index as words, therefore search only for the start part (or the attribute name)
-          int attributeStart = name.indexOf('{');
-          int attributeEnd = name.indexOf('}', attributeStart);
-          if (attributeStart > 0) {
-            nameToSearch = name.substring(0, attributeStart);
-          } else if (attributeEnd - (attributeStart + 1) > 0) {
-            nameToSearch = name.substring(attributeStart + 1, attributeEnd);
-          }
-        }
-        String nameFinal = nameToSearch;
-
-        GlobalSearchScope finalScope = (GlobalSearchScope) scope;
-        files = myDumbService.runReadActionInSmartMode(() -> {
-          short context = UsageSearchContext.IN_CODE;
-          if (element instanceof AsciiDocTagDeclaration) {
-            context = UsageSearchContext.ANY;
-          }
-          return CacheManager.getInstance(element.getProject())
-            .getFilesWithWord(nameFinal, context, finalScope, false);
-          }
-        );
-
-        if (element instanceof AsciiDocAttributeDeclarationName) {
-          Collection<AsciiDocAttributeDeclaration> asciiDocAttributeDeclarations = AsciiDocAttributeDeclarationKeyIndex.getInstance().get(name, element.getProject(),
-            // searching also the libraries is generally not useful; therefore restrict to project scope
-            ((GlobalSearchScope) scope).intersectWith(new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType())
-          );
-          for (AsciiDocAttributeDeclaration attribute : asciiDocAttributeDeclarations) {
+      }
+      ProgressManager.checkCanceled();
+      if (localSearch) {
+        for (AsciiDocAttributeDeclaration attribute : PsiTreeUtil.findChildrenOfType(psiFile, AsciiDocAttributeDeclaration.class)) {
+          if (attribute.matchesKey(name)) {
             AsciiDocAttributeDeclarationName child = attribute.getAttributeDeclarationName();
             if (child != null) {
               consumePsiElementForRename(consumer, child);
             }
           }
         }
-
-        if (files.length == 0) {
-          return;
-        }
-      } else if (scope instanceof LocalSearchScope) {
-        localSearch = true;
-        // when the IDE highlights references of the current file in the editor
-        LocalSearchScope localSearchScope = (LocalSearchScope) scope;
-        if (localSearchScope.getScope().length == 1 && localSearchScope.getScope()[0] instanceof PsiFile) {
-          files = new PsiFile[]{(PsiFile) localSearchScope.getScope()[0]};
-        } else {
-          return;
-        }
-      } else {
-        return;
       }
-
-      // default, like for: AsciiDocAttributeDeclarationName
-      boolean caseSensitive = false;
-      if (element instanceof AsciiDocBlockId || element instanceof AsciiDocTagDeclaration) {
-        caseSensitive = true;
-      }
-      final StringSearcher asciidocSearcher = new StringSearcher(name, caseSensitive, true, false);
-      final StringSearcher tagdeclarationSearcher = new StringSearcher("::" + name + "[]", caseSensitive, true, false);
-      @Nullable ProgressIndicator pi = ProgressManager.getInstance().getProgressIndicator();
-      if (pi != null) {
-        pi.setText("Searching text in " + files.length + " files");
-        pi.setIndeterminate(false);
-      }
-      for (int i = 0; i < files.length; i++) {
-        PsiFile psiFile = files[i];
-        if (pi != null) {
-          pi.setFraction((double) i / files.length);
-          VirtualFile vf = psiFile.getVirtualFile();
-          if (vf != null) {
-            pi.setText2(vf.getPresentableName());
-          } else {
-            pi.setText2(null);
-          }
-        }
+      final CharSequence text = ReadAction.compute(() -> psiFile.getViewProvider().getContents());
+      LowLevelSearchUtil.processTexts(text, 0, text.length(), psiFile.getLanguage() == AsciiDocLanguage.INSTANCE ? asciidocSearcher : tagdeclarationSearcher, index -> {
         ProgressManager.checkCanceled();
-        if (localSearch) {
-          for (AsciiDocAttributeDeclaration attribute : PsiTreeUtil.findChildrenOfType(psiFile, AsciiDocAttributeDeclaration.class)) {
-            if (attribute.matchesKey(name)) {
-              AsciiDocAttributeDeclarationName child = attribute.getAttributeDeclarationName();
-              if (child != null) {
-                consumePsiElementForRename(consumer, child);
-              }
+        myDumbService.runReadActionInSmartMode(() -> {
+          PsiReference referenceAt = psiFile.findReferenceAt(index + (psiFile.getLanguage() == AsciiDocLanguage.INSTANCE ? 0 : 2));
+          if (referenceAt instanceof PsiMultiReference) {
+            for (PsiReference reference : ((PsiMultiReference) referenceAt).getReferences()) {
+              checkReference(consumer, element, reference);
             }
           }
-        }
-        final CharSequence text = ReadAction.compute(() -> psiFile.getViewProvider().getContents());
-        LowLevelSearchUtil.processTexts(text, 0, text.length(), psiFile.getLanguage() == AsciiDocLanguage.INSTANCE ? asciidocSearcher : tagdeclarationSearcher, index -> {
-          ProgressManager.checkCanceled();
-          myDumbService.runReadActionInSmartMode(() -> {
-            PsiReference referenceAt = psiFile.findReferenceAt(index + (psiFile.getLanguage() == AsciiDocLanguage.INSTANCE ? 0 : 2));
-            if (referenceAt instanceof PsiMultiReference) {
-              for (PsiReference reference : ((PsiMultiReference) referenceAt).getReferences()) {
-                checkReference(consumer, element, reference);
-              }
-            }
-            checkReference(consumer, element, referenceAt);
-          });
-          return true;
+          checkReference(consumer, element, referenceAt);
         });
-        if (pi != null) {
-          pi.setText2(null);
-        }
+        return true;
+      });
+      if (pi != null) {
+        pi.setText2(null);
       }
-    });
+    }
+
   }
 
   private void consumePsiElementForRename(@NotNull Processor<? super PsiReference> consumer, PsiNamedElement element) {
--- src/main/java/org/asciidoc/intellij/findUsages/AsciiDocJavaReferencesSearch.java
+++ src/main/java/org/asciidoc/intellij/findUsages/AsciiDocJavaReferencesSearch.java
@@ -35,87 +35,91 @@ public class AsciiDocJavaReferencesSearch extends QueryExecutorBase<PsiReference
   @Override
   public void processQuery(@NotNull ReferencesSearch.SearchParameters p, @NotNull Processor<? super PsiReference> consumer) {
     final PsiElement element = p.getElementToSearch();
-    if (!element.isValid()) {
+    if (!(element instanceof PsiClass) && !(element instanceof PsiPackage)) {
       return;
     }
-
-    // use scope determined by user here, as effective search scope would return the module and its dependants
-    SearchScope scope = p.getScopeDeterminedByUser();
-
-    if (element instanceof PsiClass) {
-      String name = ((PsiClass) element).getName();
-      if (name != null && !name.isEmpty()) {
-        search(consumer, element, name, scope);
+    AsciiDocProcessUtil.runInReadActionWithWriteActionPriority(() -> {
+      if (!element.isValid()) {
+        return;
       }
-    } else if (element instanceof PsiPackage) {
-      String name = ((PsiPackage) element).getName();
-      if (name != null && !name.isEmpty()) {
-        search(consumer, element, name, scope);
+
+      // use scope determined by user here, as effective search scope would return the module and its dependants
+      SearchScope scope = p.getScopeDeterminedByUser();
+
+      if (element instanceof PsiClass) {
+        String name = ((PsiClass) element).getName();
+        if (name != null && !name.isEmpty()) {
+          search(consumer, element, name, scope);
+        }
+      } else //noinspection ConstantConditions
+        if (element instanceof PsiPackage) {
+        String name = ((PsiPackage) element).getName();
+        if (name != null && !name.isEmpty()) {
+          search(consumer, element, name, scope);
+        }
       }
-    }
+    });
   }
 
   private void search(@NotNull Processor<? super PsiReference> consumer, PsiElement element, String name, SearchScope scope) {
-    AsciiDocProcessUtil.runInReadActionWithWriteActionPriority(() -> {
-      DumbService myDumbService = DumbService.getInstance(element.getProject());
-      PsiFile[] files;
-      if (scope instanceof GlobalSearchScope) {
-        // when the user searches all references
-        files = myDumbService.runReadActionInSmartMode(() ->
-          CacheManager.getInstance(element.getProject())
-            .getFilesWithWord(name, UsageSearchContext.IN_CODE, (GlobalSearchScope) scope, false));
-        if (files.length == 0) {
-          return;
-        }
-      } else if (scope instanceof LocalSearchScope) {
-        // when the IDE highlights references of the current file in the editor
-        LocalSearchScope localSearchScope = (LocalSearchScope) scope;
-        if (localSearchScope.getScope().length == 1 && localSearchScope.getScope()[0] instanceof PsiFile) {
-          files = new PsiFile[] {(PsiFile) localSearchScope.getScope()[0]};
-        } else {
-          return;
-        }
+    DumbService myDumbService = DumbService.getInstance(element.getProject());
+    PsiFile[] files;
+    if (scope instanceof GlobalSearchScope) {
+      // when the user searches all references
+      files = myDumbService.runReadActionInSmartMode(() ->
+        CacheManager.getInstance(element.getProject())
+          .getFilesWithWord(name, UsageSearchContext.IN_CODE, (GlobalSearchScope) scope, false));
+      if (files.length == 0) {
+        return;
+      }
+    } else if (scope instanceof LocalSearchScope) {
+      // when the IDE highlights references of the current file in the editor
+      LocalSearchScope localSearchScope = (LocalSearchScope) scope;
+      if (localSearchScope.getScope().length == 1 && localSearchScope.getScope()[0] instanceof PsiFile) {
+        files = new PsiFile[] {(PsiFile) localSearchScope.getScope()[0]};
       } else {
         return;
       }
-      final StringSearcher searcher = new StringSearcher(name, true, true, false);
-      @Nullable ProgressIndicator pi = ProgressManager.getInstance().getProgressIndicator();
+    } else {
+      return;
+    }
+    final StringSearcher searcher = new StringSearcher(name, true, true, false);
+    @Nullable ProgressIndicator pi = ProgressManager.getInstance().getProgressIndicator();
+    if (pi != null) {
+      pi.setText("Searching text in " + files.length + " files");
+      pi.setIndeterminate(false);
+    }
+    for (int i = 0; i < files.length; i++) {
+      PsiFile psiFile = files[i];
       if (pi != null) {
-        pi.setText("Searching text in " + files.length + " files");
-        pi.setIndeterminate(false);
-      }
-      for (int i = 0; i < files.length; i++) {
-        PsiFile psiFile = files[i];
-        if (pi != null) {
-          pi.setFraction((double) i / files.length);
-          VirtualFile vf = psiFile.getVirtualFile();
-          if (vf != null) {
-            pi.setText2(vf.getPresentableName());
-          } else {
-            pi.setText2(null);
-          }
+        pi.setFraction((double) i / files.length);
+        VirtualFile vf = psiFile.getVirtualFile();
+        if (vf != null) {
+          pi.setText2(vf.getPresentableName());
+        } else {
+          pi.setText2(null);
         }
-        ProgressManager.checkCanceled();
-        if (psiFile.getLanguage() == AsciiDocLanguage.INSTANCE) {
-          final CharSequence text = ReadAction.compute(() -> psiFile.getViewProvider().getContents());
-          LowLevelSearchUtil.processTexts(text, 0, text.length(), searcher, index -> {
-            myDumbService.runReadActionInSmartMode(() -> {
-              PsiReference referenceAt = psiFile.findReferenceAt(index);
-              if (referenceAt instanceof PsiMultiReference) {
-                for (PsiReference reference : ((PsiMultiReference) referenceAt).getReferences()) {
-                  checkReference(consumer, element, reference);
-                }
+      }
+      ProgressManager.checkCanceled();
+      if (psiFile.getLanguage() == AsciiDocLanguage.INSTANCE) {
+        final CharSequence text = ReadAction.compute(() -> psiFile.getViewProvider().getContents());
+        LowLevelSearchUtil.processTexts(text, 0, text.length(), searcher, index -> {
+          myDumbService.runReadActionInSmartMode(() -> {
+            PsiReference referenceAt = psiFile.findReferenceAt(index);
+            if (referenceAt instanceof PsiMultiReference) {
+              for (PsiReference reference : ((PsiMultiReference) referenceAt).getReferences()) {
+                checkReference(consumer, element, reference);
               }
-              checkReference(consumer, element, referenceAt);
-            });
-            return true;
+            }
+            checkReference(consumer, element, referenceAt);
           });
-        }
+          return true;
+        });
       }
-      if (pi != null) {
-        pi.setText2(null);
-      }
-    });
+    }
+    if (pi != null) {
+      pi.setText2(null);
+    }
   }
 
   private void checkReference(@NotNull Processor<? super PsiReference> consumer, PsiElement element, PsiReference reference) {
