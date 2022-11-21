50017d04935f15e65329db6b6995010df616ab70
Alexander Schwartz <alexander.schwartz@gmx.net>
2022-11-21 18:08:48 +0100

    Rename search scope _All files without Symlinks_ to _Project files without Symlinks_ (#1216)

--- src/main/java/org/asciidoc/intellij/searchScopes/AsciiDocSearchScopeProvider.java
+++ src/main/java/org/asciidoc/intellij/searchScopes/AsciiDocSearchScopeProvider.java
@@ -20,7 +20,7 @@ public class AsciiDocSearchScopeProvider implements SearchScopeProvider {
   @Override
   public @NotNull List<SearchScope> getSearchScopes(@NotNull Project project, @NotNull DataContext dataContext) {
     ArrayList<SearchScope> result = new ArrayList<>();
-    result.add(new AsciiDocSearchScopeWithName(new AsciiDocSearchScope(project).excludeSymlinks(), "All files without Symlinks"));
+    result.add(new AsciiDocSearchScopeWithName(new AsciiDocSearchScope(project).excludeSymlinks(), "Project files without Symlinks"));
     result.add(new AsciiDocSearchScopeWithName(new AsciiDocSearchScope(project).restrictedByAsciiDocFileType(), "AsciiDoc files"));
     result.add(new AsciiDocSearchScopeWithName(new AsciiDocSearchScope(project).excludeSymlinks().restrictedByAsciiDocFileType(), "AsciiDoc files without Symlinks"));
     return result;
