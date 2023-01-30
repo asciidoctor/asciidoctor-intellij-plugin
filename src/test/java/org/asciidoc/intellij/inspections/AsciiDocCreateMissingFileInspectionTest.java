03e0ac01e9a23cbe6035d49570875a8707672e33
Alexander Schwartz <alexander.schwartz@gmx.net>
2023-01-30 21:30:15 +0100

    ensure that the test runs on different windows drive setups

--- src/test/java/org/asciidoc/intellij/inspections/AsciiDocCreateMissingFileInspectionTest.java
+++ src/test/java/org/asciidoc/intellij/inspections/AsciiDocCreateMissingFileInspectionTest.java
@@ -1,8 +1,13 @@
 package org.asciidoc.intellij.inspections;
 
 import com.intellij.psi.PsiDirectory;
+import org.apache.commons.io.FileUtils;
 import org.asciidoc.intellij.quickfix.AsciiDocCreateMissingFileIntentionAction;
 import org.junit.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class AsciiDocCreateMissingFileInspectionTest extends AsciiDocQuickFixTestBase {
 
@@ -24,11 +29,20 @@ public class AsciiDocCreateMissingFileInspectionTest extends AsciiDocQuickFixTes
 
   @Override
   protected String getBasePath() {
-    // highlighting contains a "C:" in the path name on windows, therefore different fixtures
-    if (System.getProperty("os.name").toLowerCase().contains("win")) {
-      return "inspections/missingfile-win";
-    } else {
-      return "inspections/missingfile";
+    try {
+      File path = new File("build/testdata/missingfile");
+      FileUtils.deleteDirectory(path);
+      FileUtils.forceMkdir(path);
+      FileUtils.copyDirectory(new File("testdata/inspections/missingfile"), new File("build/testdata/missingfile"));
+      if (System.getProperty("os.name").toLowerCase().contains("win")) {
+        // highlighting contains a "C:" in the path name on windows, therefore the fixture needs to be patched
+        String contents = FileUtils.readFileToString(new File("build/testdata/missingfile/createMissingIncludeFile.adoc"), StandardCharsets.UTF_8);
+        contents = contents.replaceAll("/src", new File("build/testdata/missingfile/createMissingIncludeFile.adoc").getAbsolutePath().charAt(0) + ":" + "/src");
+        FileUtils.writeStringToFile(new File("build/testdata/missingfile/createMissingIncludeFile.adoc"), contents, StandardCharsets.UTF_8);
+      }
+      return "../build/testdata/missingfile";
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
