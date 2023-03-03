513ebd02a9ce56e62d927dd0690408d8e662ac75
Alexander Schwartz <alexander.schwartz@gmx.net>
2023-03-03 23:14:35 +0100

    remove obsolete code for a workaround no longer needed

--- src/main/java/org/asciidoc/intellij/editor/AsciiDocSplitEditorProvider.java
+++ src/main/java/org/asciidoc/intellij/editor/AsciiDocSplitEditorProvider.java
@@ -1,15 +1,10 @@
 package org.asciidoc.intellij.editor;
 
-import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.TextEditor;
-import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
 import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorProvider;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.module.impl.ModuleManagerEx;
-import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.Disposer;
@@ -23,13 +18,6 @@ import org.jetbrains.annotations.NotNull;
 public class AsciiDocSplitEditorProvider extends SplitTextEditorProvider {
   public AsciiDocSplitEditorProvider() {
     super(new PsiAwareTextEditorProvider(), new AsciiDocPreviewEditorProvider());
-
-    // removing the entry from the history prevents an "Unable to find providerId=text-editor" assertion error
-    // in EditorComposite
-    // https://youtrack.jetbrains.com/issue/IDEA-289732 - gone in 2022.1 final
-    boolean shouldClearEditorHistory = ApplicationInfo.getInstance().getBuild().getBaselineVersion() == 221
-       && ApplicationInfo.getInstance().getBuild().getComponents().length > 1
-       && ApplicationInfo.getInstance().getBuild().getComponents()[1] < 5080;
 
     // when this plugin is installed at runtime, check if the existing editors as split editors.
     // If not, close the editor and re-open the files
@@ -50,42 +38,12 @@ public class AsciiDocSplitEditorProvider extends SplitTextEditorProvider {
                   ApplicationManager.getApplication().runWriteAction(() -> {
                     // closing the file might trigger a save, therefore wrap in write action
                     fem.closeFile(vFile);
-                    if (shouldClearEditorHistory) {
-                      if (EditorHistoryManager.getInstance(project).getState(vFile, this) == null) {
-                        EditorHistoryManager.getInstance(project).removeFile(vFile);
-                      }
-                    }
                     fem.openFile(vFile, false);
                   });
                 }
               }
             }
           }
-        }
-        if (shouldClearEditorHistory) {
-          DumbService.getInstance(project).runWhenSmart(() -> ApplicationManager.getApplication().invokeLater(() -> {
-            ModuleManager manager = ModuleManager.getInstance(project);
-            if (manager instanceof ModuleManagerEx) {
-              // use this to avoid an exception in RootIndex.java:69
-              if (!((ModuleManagerEx) manager).areModulesLoaded()) {
-                return;
-              }
-            }
-
-            // clearing the history after the re-opening of files for all closed files in the history.
-            for (VirtualFile vFile : EditorHistoryManager.getInstance(project).getFileList()) {
-              if (vFile.isValid()) {
-                PsiFile pFile = pm.findFile(vFile);
-                if (pFile != null) {
-                  if (pFile.getLanguage() == AsciiDocLanguage.INSTANCE) {
-                    if (EditorHistoryManager.getInstance(project).getState(vFile, this) == null) {
-                      EditorHistoryManager.getInstance(project).removeFile(vFile);
-                    }
-                  }
-                }
-              }
-            }
-          }));
         }
       }
     });
