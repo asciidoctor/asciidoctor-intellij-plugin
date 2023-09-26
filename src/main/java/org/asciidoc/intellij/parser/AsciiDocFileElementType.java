ac876ffeb7d223598187fd109e370eb3a92411b4
Alexander Schwartz <alexander.schwartz@gmx.net>
2023-09-26 23:28:06 +0200

    Reinitialize stubs due to changes in 0.39.9

--- src/main/java/org/asciidoc/intellij/parser/AsciiDocFileElementType.java
+++ src/main/java/org/asciidoc/intellij/parser/AsciiDocFileElementType.java
@@ -13,6 +13,6 @@ public class AsciiDocFileElementType extends IStubFileElementType<PsiFileStub<As
 
   @Override
   public int getStubVersion() {
-    return super.getStubVersion() + 53;
+    return super.getStubVersion() + 54;
   }
 }
