d5b849cedc5992c31d5d3c2f689c8dbd34b9c329
Alexander Schwartz <alexander.schwartz@gmx.net>
2024-01-22 23:44:24 +0100

    Hide unused AsciiDoc action group in IntelliJ 2024.1 EAP

--- src/main/java/org/asciidoc/intellij/actions/asciidoc/AsciiDocActionGroup.java
+++ src/main/java/org/asciidoc/intellij/actions/asciidoc/AsciiDocActionGroup.java
@@ -3,8 +3,9 @@ package org.asciidoc.intellij.actions.asciidoc;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 
 public class AsciiDocActionGroup extends DefaultActionGroup {
-  @Override
-  public boolean hideIfNoVisibleChildren() {
-    return true;
+
+  {
+    getTemplatePresentation().setHideGroupIfEmpty(true);
   }
+
 }
