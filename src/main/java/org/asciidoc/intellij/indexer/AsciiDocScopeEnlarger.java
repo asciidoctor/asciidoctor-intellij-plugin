a92cc2305ccea83cff2da7b7a1c97921262d7b8e
Alexander Schwartz <alexander.schwartz@gmx.net>
2024-02-07 21:29:33 +0100

    Workaround for slow EDT warning (#1533)

--- src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
+++ src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
@@ -4,6 +4,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.UseScopeEnlarger;
+import com.intellij.util.SlowOperations;
 import org.asciidoc.intellij.psi.AsciiDocSearchScope;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,10 +14,14 @@ public class AsciiDocScopeEnlarger extends UseScopeEnlarger {
   @Override
   public SearchScope getAdditionalUseScope(@NotNull PsiElement element) {
     // not restricting to GlobalSearchScope breaks refactorings like extract-variable
-    if (element.getUseScope() instanceof GlobalSearchScope) {
-      // any element can be referenced from an AsciiDoc element within the same project (directory, file, image, or an ID in an AsciiDoc file)
-      return new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType();
-    }
-    return null;
+    SearchScope[] result = new SearchScope[1];
+    SlowOperations.allowSlowOperations(() -> {
+      // workaround for https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/1533
+      if (element.getUseScope() instanceof GlobalSearchScope) {
+        // any element can be referenced from an AsciiDoc element within the same project (directory, file, image, or an ID in an AsciiDoc file)
+        result[0] = new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType();
+      }
+    });
+    return result[0];
   }
 }
