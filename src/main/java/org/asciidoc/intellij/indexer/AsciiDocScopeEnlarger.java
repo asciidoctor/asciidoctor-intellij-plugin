22a54d225b9e2f59900b6bd028ef960244d51a8b
Alexander Schwartz <alexander.schwartz@gmx.net>
2024-03-04 21:35:35 +0100

    Remove code which must not be run in EDT to get compatible with IntelliJ 2024.1 EAP (#1533)

--- src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
+++ src/main/java/org/asciidoc/intellij/indexer/AsciiDocScopeEnlarger.java
@@ -14,14 +14,15 @@ public class AsciiDocScopeEnlarger extends UseScopeEnlarger {
   @Override
   public SearchScope getAdditionalUseScope(@NotNull PsiElement element) {
     // not restricting to GlobalSearchScope breaks refactorings like extract-variable
-    SearchScope[] result = new SearchScope[1];
-    SlowOperations.allowSlowOperations(() -> {
-      // workaround for https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/1533
-      if (element.getUseScope() instanceof GlobalSearchScope) {
-        // any element can be referenced from an AsciiDoc element within the same project (directory, file, image, or an ID in an AsciiDoc file)
-        result[0] = new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType();
-      }
-    });
-    return result[0];
+    // Leads to EDT problems with EAP 2024.1.
+    // Allowing via SlowOperations.allowSlowOperations() as a workaround in
+    // https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/1533
+    // doesn't work as expected. The original cause for this check as described in https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/466
+    // seems no longer present - therefore removing the check
+    // if (element.getUseScope() instanceof GlobalSearchScope) {
+    // any element can be referenced from an AsciiDoc element within the same project (directory, file, image, or an ID in an AsciiDoc file)
+    return new AsciiDocSearchScope(element.getProject()).restrictedByAsciiDocFileType();
+    // }
+    // return null;
   }
 }
