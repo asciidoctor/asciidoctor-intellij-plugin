6de5af66a0876b650552c24934fa38227de350d7
Alexander Schwartz <aschwart@redhat.com>
2024-05-02 23:16:40 +0200

    Automatically clear lock for JCEF preview to prevent a blank preview (#1610)

--- src/main/java/org/asciidoc/intellij/editor/jcef/AsciiDocJCEFHtmlPanelProvider.java
+++ src/main/java/org/asciidoc/intellij/editor/jcef/AsciiDocJCEFHtmlPanelProvider.java
@@ -1,20 +1,56 @@
 package org.asciidoc.intellij.editor.jcef;
 
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.ui.jcef.JBCefApp;
+import org.asciidoc.intellij.download.AsciiDocDownloaderUtil;
 import org.asciidoc.intellij.editor.AsciiDocHtmlPanel;
 import org.asciidoc.intellij.editor.AsciiDocHtmlPanelProvider;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class AsciiDocJCEFHtmlPanelProvider extends AsciiDocHtmlPanelProvider {
   public static final ProviderInfo INFO = new ProviderInfo("JCEF Browser", AsciiDocJCEFHtmlPanelProvider.class.getName());
 
+  private static final Logger LOG = Logger.getInstance(AsciiDocDownloaderUtil.class);
+  private static boolean staleCheckComplete;
+
   @NotNull
   @Override
   public AsciiDocHtmlPanel createHtmlPanel(Document document, Path imagesPath, Runnable forceRefresh) {
+    clearStaleJCEFlock();
     return new AsciiDocJCEFHtmlPanel(document, imagesPath, forceRefresh);
+  }
+
+  private static synchronized void clearStaleJCEFlock() {
+    // Workaround for https://youtrack.jetbrains.com/issue/IJPL-148653,
+    // when a lock file remains which the name of the old host name
+    if (!staleCheckComplete) {
+      staleCheckComplete = true;
+      Path jcefCache = PathManager.getSystemDir().resolve("jcef_cache");
+      if (jcefCache.toFile().exists()) {
+        Path singletonLock = jcefCache.resolve("SingletonLock");
+        try {
+          String hostName = InetAddress.getLocalHost().getHostName();
+          String lockFile = Files.readSymbolicLink(singletonLock).toFile().getName();
+          if (!lockFile.matches(Pattern.quote(hostName) + "-[0-9]*")) {
+            // delete the stale lock to prevent the preview from appearing blank
+            Files.delete(singletonLock);
+          }
+        } catch (NoSuchFileException ignore) {
+          // nop
+        } catch (IOException e) {
+          LOG.warn("Can't check lock", e);
+        }
+      }
+    }
   }
 
   @NotNull
