2eaf705dfdc72054deacd4c75044c84404ee2dad
Alexander Schwartz <aschwart@redhat.com>
2024-11-04 22:49:10 +0100

    Simplify code thanks to hint from Yann

---
+++ src/main/java/org/asciidoc/intellij/activities/AsciiDocHandleUnloadEvent.java
@@ -0,0 +1,92 @@
+package org.asciidoc.intellij.activities;
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.DaemonProgressIndicator;
+import com.intellij.ide.plugins.CannotUnloadPluginException;
+import com.intellij.ide.plugins.DynamicPluginListener;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.asciidoc.intellij.AsciiDocPlugin;
+import org.asciidoc.intellij.AsciiDocWrapper;
+import org.asciidoc.intellij.editor.AsciiDocSplitEditor;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * This takes care of unloading the plugin on uninstalls or updates.
+ * WARNING: A dynamic unload will usually only succeed when the application is NOT in debug mode;
+ * classes might be marked as "JNI Global" due to this, and not reclaimed, and then unloading fails.
+ */
+public class AsciiDocHandleUnloadEvent implements DynamicPluginListener {
+
+  private static final com.intellij.openapi.diagnostic.Logger LOG =
+    com.intellij.openapi.diagnostic.Logger.getInstance(AsciiDocHandleUnloadEvent.class);
+
+  @Override
+  public void checkUnloadPlugin(@NotNull IdeaPluginDescriptor pluginDescriptor) throws CannotUnloadPluginException {
+    if (Objects.equals(pluginDescriptor.getPluginId().getIdString(), AsciiDocPlugin.PLUGIN_ID)) {
+      LOG.info("checkUnloadPlugin");
+      // https://github.com/asciidoctor/asciidoctor-intellij-plugin/issues/512
+      // another reason: on Windows even after unloading JAR file of the plugin still be locked and can't be deleted, making uninstall impossible
+      // https://youtrack.jetbrains.com/issue/IDEA-244471
+      // Update: IDEA-244471 might not be relevant here as the plugin will have the version number in the JAR file, therefore a change file will have a new name
+
+      // before trying to re-enable this for internal mode, try to unload plugin in development mode and analyze heap dumps.
+      // if (!ApplicationManager.getApplication().isInternal()) {
+      throw new CannotUnloadPluginException("unloading mechanism is not safe, incomplete unloading might lead to strange exceptions");
+      // }
+      // Pending: https://youtrack.jetbrains.com/issue/IJPL-18535/, https://youtrack.jetbrains.com/issue/IJPL-166041/, https://youtrack.jetbrains.com/issue/IJPL-166040/
+      // AsciiDoc.checkUnloadPlugin();
+    }
+  }
+
+  @Override
+  public void beforePluginUnload(@NotNull IdeaPluginDescriptor pluginDescriptor, boolean isUpdate) {
+    if (Objects.equals(pluginDescriptor.getPluginId().getIdString(), AsciiDocPlugin.PLUGIN_ID)) {
+      LOG.info("beforePluginUnload");
+      AsciiDocWrapper.beforePluginUnload();
+      for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+
+        // Workaround for https://youtrack.jetbrains.com/issue/IJPL-18535/
+        try {
+          DaemonCodeAnalyzer dca = DaemonCodeAnalyzer.getInstance(project);
+          Field myUpdateProgress = DaemonCodeAnalyzerImpl.class.getDeclaredField("myUpdateProgress");
+          myUpdateProgress.setAccessible(true);
+          Map<FileEditor, DaemonProgressIndicator> map = (Map<FileEditor, DaemonProgressIndicator>) myUpdateProgress.get(dca);
+          map.entrySet().clear();
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+          // nopp
+        }
+
+        // Possibly not necessary in the future if IntelliJ doesn't hold on to references
+        FileEditorManager fem = FileEditorManager.getInstance(project);
+        for (FileEditor editor : fem.getAllEditors()) {
+          if ((editor instanceof AsciiDocSplitEditor)) {
+            ApplicationManager.getApplication().runReadAction(() -> {
+              VirtualFile vFile = editor.getFile();
+              if (vFile != null && vFile.isValid()) {
+                // an AsciiDoc file in a non-split editor, close and re-open the file to enforce split editor
+                ApplicationManager.getApplication().runWriteAction(() -> {
+                  // closing the file might trigger a save, therefore, wrap in write action
+                  if (!project.isDisposed()) {
+                    fem.closeFile(vFile);
+                  }
+                });
+              }
+            });
+          }
+        }
+      }
+    }
+  }
+
+}
