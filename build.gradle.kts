499b8d0a7f2ea5bd9b47558de2e13c20b5c5da16
Alexander Schwartz <aschwart@redhat.com>
2024-12-21 16:51:45 +0100

    Upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ dependencies {
             // "com.jetbrains.performancePlugin:213.5744.122" // used run YourKit CPU profiling in test IDE
             // see https://intellij-support.jetbrains.com/hc/en-us/articles/207241235
             // https://plugins.jetbrains.com/plugin/16136-grazie-professional/
-            "com.intellij.grazie.pro:0.3.320"
+            "com.intellij.grazie.pro:0.3.354"
         ))
 
         instrumentationTools()
