4e644cedf8d735adc032c796296a72bf8d490e7d
Alexander Schwartz <aschwart@redhat.com>
2025-03-15 09:58:46 +0100

    Consolidating versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,15 +66,13 @@ dependencies {
 
     testImplementation(gradleTestKit())
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.0")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.12.0")
+    testRuntimeOnly(libs.junitJupiterEngine)
+    testRuntimeOnly(libs.junitVintageEngine)
 
     testImplementation("com.tngtech.archunit:archunit-junit4:1.4.0")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.mockito:mockito-core:5.16.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.12.0")
+    testImplementation(libs.junitJupiterApi)
 
     // implementation(libs.exampleLibrary)
 
