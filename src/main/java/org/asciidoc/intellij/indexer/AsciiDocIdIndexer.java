f81d850d196ae82ce8fddf737d5fc5b4aa332bb3
Alexander Schwartz <alexander.schwartz@gmx.net>
2026-09-13 18:09:41 +0200

    Bump version to force reindexing
    Follow-up to change 0c16a6d81ac97af6f9268a616d843cf871ba297f

--- src/main/java/org/asciidoc/intellij/indexer/AsciiDocIdIndexer.java
+++ src/main/java/org/asciidoc/intellij/indexer/AsciiDocIdIndexer.java
@@ -13,7 +13,7 @@ public class AsciiDocIdIndexer extends LexerBasedIdIndexer {
 
   @Override
   public int getVersion() {
-    return 40;
+    return 41;
   }
 
   public static Lexer createIndexingLexer(OccurrenceConsumer consumer) {
--- src/main/java/org/asciidoc/intellij/indexer/AsciiDocTodoIndexer.java
+++ src/main/java/org/asciidoc/intellij/indexer/AsciiDocTodoIndexer.java
@@ -12,7 +12,7 @@ public class AsciiDocTodoIndexer extends LexerBasedTodoIndexer {
 
   @Override
   public int getVersion() {
-    return 29;
+    return 30;
   }
 
   @NotNull
